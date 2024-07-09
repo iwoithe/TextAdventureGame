46cf5a54deba17fd62046fedec64ebf943461ba1
Ilias Woithe <iwoithe@just42.net>
2024-07-09 15:03:08 +0930

    Add todo

--- src/spells/spells.cpp
+++ src/spells/spells.cpp
@@ -6,6 +6,7 @@
 
 ISpell* getRandomSpell()
 {
+    // TODO: Stop the player from getting the same spell twice (one instance is enough)
     int lowerLimit = 0;
     int upperLimit = SPELL_TYPE_LENGTH;
     int randInt = rand() % (upperLimit - lowerLimit) + lowerLimit;
